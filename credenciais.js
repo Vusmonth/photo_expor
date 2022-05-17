583b392ea12dac95b87f8fcf02ecc7fd3528983e
Vusmonth <39846007+Vusmonth@users.noreply.github.com>
2022-05-17 10:01:18 -0300

    ref: credenciais do firebase atualizadas

--- credenciais.js
+++ credenciais.js
@@ -1,8 +1,8 @@
 export const firebaseAuth = {
-    apiKey: "AIzaSyA6Dorc4cx28oqz8boBsQiiNsgCY79K5OQ",
-    authDomain: "photo-expor.firebaseapp.com",
-    projectId: "photo-expor",
-    storageBucket: "photo-expor.appspot.com",
-    messagingSenderId: "168916897541",
-    appId: "1:168916897541:web:457f2f77ff96c0863cf16c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
